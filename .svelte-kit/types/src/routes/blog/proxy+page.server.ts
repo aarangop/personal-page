38a4236bc63c97edf567d5b2934b2426326b7cd4
Andrés Arango Pérez <a.arango@tu-braunschweig.de>
2024-04-17 13:42:22 +0200

    Vercel GCP Credentials

---
+++ .svelte-kit/types/src/routes/blog/proxy+page.server.ts
@@ -0,0 +1,10 @@
+// @ts-nocheck
+import type { PageServerLoad } from './$types';
+import { BlogPostSchema } from '$lib/schemas';
+import { z } from 'zod';
+
+export const load = async ({ fetch }: Parameters<PageServerLoad>[0]) => ({
+	posts: await fetch('/api/blogs')
+		.then((response) => response.json())
+		.then((data) => data)
+});
