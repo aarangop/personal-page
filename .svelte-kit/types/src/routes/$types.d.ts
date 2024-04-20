07afd46cf8b7dadc3a1e62657b2722f95ba8b88f
Andrés Arango Pérez <a.arango@tu-braunschweig.de>
2024-04-20 09:24:24 +0200

    Add github authentication

--- .svelte-kit/types/src/routes/$types.d.ts
+++ .svelte-kit/types/src/routes/$types.d.ts
@@ -12,11 +12,15 @@ type EnsureDefined<T> = T extends null | undefined ? {} : T;
 type OptionalUnion<U extends Record<string, any>, A extends keyof U = U extends U ? keyof U : never> = U extends unknown ? { [P in Exclude<A, keyof U>]?: never } & U : never;
 export type Snapshot<T = any> = Kit.Snapshot<T>;
 type PageParentData = EnsureDefined<LayoutData>;
-type LayoutRouteId = RouteId | "/" | "/admin" | "/admin/blog" | "/admin/blog/[slug]" | "/admin/blog/new" | "/admin/podcast" | "/admin/podcast/edit/[slug]" | "/admin/podcast/new" | "/blog" | "/blog/[slug]" | "/podcast" | "/podcast/[slug]" | null
+type LayoutRouteId = RouteId | "/" | "/admin" | "/admin/blog" | "/admin/blog/[slug]" | "/admin/blog/new" | "/admin/podcast" | "/admin/podcast/edit/[slug]" | "/admin/podcast/new" | "/blog" | "/blog/[slug]" | "/podcast" | "/podcast/[slug]" | "/signin" | null
 type LayoutParams = RouteParams & { slug?: string }
+type LayoutServerParentData = EnsureDefined<{}>;
 type LayoutParentData = EnsureDefined<{}>;
 
 export type PageServerData = null;
 export type PageData = Expand<PageParentData>;
-export type LayoutServerData = null;
-export type LayoutData = Expand<LayoutParentData>;
+export type LayoutServerLoad<OutputData extends OutputDataShape<LayoutServerParentData> = OutputDataShape<LayoutServerParentData>> = Kit.ServerLoad<LayoutParams, LayoutServerParentData, OutputData, LayoutRouteId>;
+export type LayoutServerLoadEvent = Parameters<LayoutServerLoad>[0];
+export type LayoutServerData = Expand<OptionalUnion<EnsureDefined<Kit.LoadProperties<Awaited<ReturnType<typeof import('./proxy+layout.server.js').load>>>>>>;
+export type LayoutData = Expand<Omit<LayoutParentData, keyof LayoutServerData> & EnsureDefined<LayoutServerData>>;
+export type RequestEvent = Kit.RequestEvent<RouteParams, RouteId>;
--- src/routes/admin/+page.svelte
+++ src/routes/admin/+page.svelte
@@ -1,4 +1,9 @@
+<script>
+	import { SignIn } from '@auth/sveltekit/components';
+</script>
+
 <div class="flex flex-row">
+	<SignIn provider="github" signInPage="signin" />
 	<a class="card p-4" href="/admin/podcast"> Podcast </a>
 	<a class="card p-4" href="/admin/blog">Blog</a>
 </div>
