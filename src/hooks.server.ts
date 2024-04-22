79a8697ed385834b10ade84e12e77430842e95c7
Andrés Arango Pérez <a.arango@tu-braunschweig.de>
2024-04-22 13:04:43 +0200

    Login/logout elements

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,7 +1,5 @@
 import { type Handle } from '@sveltejs/kit';
 import { sequence } from '@sveltejs/kit/hooks';
-
 import { protectPrivateRoutesHandle } from '$lib/server/auth';
 import { handle as authHandle } from './auth';
-
 export const handle: Handle = sequence(authHandle, protectPrivateRoutesHandle);
