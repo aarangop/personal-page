75c6593eda3e00f250d71a1a14edd354b7326277
Andrés Arango Pérez <a.arango@tu-braunschweig.de>
2024-04-24 16:02:16 +0200

    Credentials authentication provider

--- .svelte-kit/output/client/_app/immutable/entry/start.DV726Fo2.js
+++
@@ -1,1 +0,0 @@
-import{b as a}from"../chunks/entry.C5BBI4Ia.js";export{a as start};
--- .svelte-kit/types/src/routes/proxy+layout.server.ts
+++ .svelte-kit/types/src/routes/proxy+layout.server.ts
@@ -1,8 +1,8 @@
 // @ts-nocheck
+import logger from '$lib/server/logging';
 import type { LayoutServerLoad } from './$types';
 
 export const load = async (event: Parameters<LayoutServerLoad>[0]) => {
 	const session = await event.locals.auth();
-
 	return { session };
 };
