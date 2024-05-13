8bc2fe196491ec0848af6fb99f920b909bd47515
Andrés Arango Pérez <a.arango@tu-braunschweig.de>
2024-05-13 12:06:39 +0200

    Remove svelte-kit directory

--- .svelte-kit/types/src/routes/admin/blog/proxy+page.server.ts
+++
@@ -1,8 +0,0 @@
-// @ts-nocheck
-import { getBlogPosts } from '$lib/server/actions/blog';
-import type { PageServerLoad } from './$types';
-
-export const load = async () => ({
-	blogPosts: await getBlogPosts()
-});
-;null as any as PageServerLoad;
