7ec8ce74b6bfb0fa3fbd292371b56e8a9a8cdf22
Andrés Arango Pérez <a.arango@tu-braunschweig.de>
2024-05-13 11:23:23 +0200

    remove .vercel directory

--- .vercel/output/functions/fn.func/.svelte-kit/output/server/chunks/ReaderLayout.js
+++
@@ -1,7 +0,0 @@
-import { c as create_ssr_component } from "./ssr.js";
-const ReaderLayout = create_ssr_component(($$result, $$props, $$bindings, slots) => {
-  return `<div class="w-full md:w-4/5 lg:w-3/5 xl:w-2/3 2xl:w-1/3 justify-center">${slots.default ? slots.default({}) : ``}</div>`;
-});
-export {
-  ReaderLayout as R
-};
--- .vercel/output/static/_app/immutable/entry/start.j3yOz19Y.js
+++
@@ -1,1 +0,0 @@
-import{b as a}from"../chunks/entry.DAzvZ7mJ.js";export{a as start};
