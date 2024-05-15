cfed65e0bc32d40c2a39c03d8c780ad8698a79a7
Andrés Arango Pérez <a.arango@tu-braunschweig.de>
2024-05-15 09:07:16 +0200

    Refactor podcast and admin/podcast to actions. Amended.

--- src/routes/admin/podcast/+page.server.ts
+++ src/routes/admin/podcast/+page.server.ts
@@ -1,6 +1,14 @@
+<<<<<<< HEAD
 import { getPodcastFeedMetaData } from '$lib/server/actions/podcast';
 import type { PageServerLoad } from './$types';
 
 export const load: PageServerLoad = async () => ({
 	podcastFeeds: await getPodcastFeedMetaData()
+=======
+import { getPodcastFeedsWithMetaData } from '$lib/server/actions/podcast';
+import type { PageServerLoad } from './$types';
+
+export const load: PageServerLoad = async () => ({
+	podcastFeeds: await getPodcastFeedsWithMetaData()
+>>>>>>> ec34272 (Refactor podcast and admin/podcast to actions. Amended.)
 });
