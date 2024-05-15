1b43061ce66f1dab3fb3c564e74c7c48d3a9433c
Andrés Arango Pérez <a.arango@tu-braunschweig.de>
2024-05-15 10:19:12 +0200

    implement and test admin/podcast/new

--- src/lib/server/actions/podcast.ts
+++ src/lib/server/actions/podcast.ts
@@ -5,6 +5,7 @@ import { error } from '@sveltejs/kit';
 import { XMLParser } from 'fast-xml-parser';
 import { z } from 'zod';
 import logger from '../logging';
+import { error, json, type RequestEvent } from '@sveltejs/kit';
 
 /**
  * Returns a list of podcast feeds from the database.
