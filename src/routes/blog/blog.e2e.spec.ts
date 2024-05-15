0da305de1e40d4f9e34155efb94c741bb348ef96
Andrés Arango Pérez <a.arango@tu-braunschweig.de>
2024-05-15 10:26:15 +0200

    Revert "Refactor podcast and admin/podcast to actions"
    This reverts commit 9abf989d3e4ee4950ca9f974d0fa875c80a7bf06, which leaked session data from e2e tests

--- src/routes/blog/blog.e2e.spec.ts
+++ src/routes/blog/blog.e2e.spec.ts
@@ -2,17 +2,30 @@ import prisma from '$lib/prisma';
 import { getRandomBlogPost } from '$lib/testing/blog';
 import test, { expect } from '@playwright/test';
 
-// Since this is a public page, the user does not require authentication.
-// So, reset storage state for this file to avoid being authenticated
-test.use({ storageState: { cookies: [], origins: [] } });
+let postIds: string[] = [];
+const nPosts = 5;
 
 test.describe('Blog page', () => {
-	// During seeding, we create a blog post. The seed script runs post install. So there should always be at least one blog post that will be shown.
+	test.beforeAll(async () => {
+		// Create sample blog post, so we can test the blog page
+		const blogPosts = Array.from({ length: nPosts }, () => getRandomBlogPost());
+		// Doing it this way because prisma does not return the ids of the created records.
+		const createPromise = blogPosts.map(
+			async (post) => await prisma.blogPost.create({ data: post })
+		);
+		// Save these ids so we can delete them after the tests
+		postIds = (await Promise.all(createPromise)).map((post) => post.id);
+	});
+
 	test('should display a list of blog posts', async ({ page }) => {
 		await page.goto('/blog');
 
 		const blogPostElements = await page.$$('data-testid=blog-post');
 
 		expect(blogPostElements.length).toBeGreaterThanOrEqual(1);
+	});
+
+	test.afterAll(async () => {
+		await prisma.blogPost.deleteMany({ where: { id: { in: postIds } } });
 	});
 });
